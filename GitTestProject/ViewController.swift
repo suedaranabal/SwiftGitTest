a291cdfbcbaee0bb72819215a1771b7fcddf9f2d
Süeda Rana Bal <suedaranabal@gmail.com>
2020-05-13 16:49:43 -0700

    Swift Project Git Deneme

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Sueda Rana Bal on 13.05.2020.
+//  Copyright © 2020 Sueda Rana Bal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("git")
+        print("deneme")
+    }
+
+
+}
+
